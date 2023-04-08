476d503cbcb2763771223384f253054488c8a7e3
Matheus Silva <matheussilva.fga@gmail.com>
2023-04-08 19:22:18 -0300

    feat: connect to mongo database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
-const port = 3001;
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+
+  })
+  .catch(() => console.log('Erro ao conectar ao mongo'));
+
+
